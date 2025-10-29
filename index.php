5d473b0ebe8d8a43205615ff1f1ddf88613ac88b
Carlos Alfaro <carlosalfaro.info@gmail.com>
2025-10-28 18:11:49 -0600

    Video #6

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    require_once "./config/app.php";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php require_once "./app/views/inc/head.php"; ?>
+</head>
+<body>
+
+    <!-- Main container -->
+    <main class="page-container">
+
+        <?php require_once "./app/views/inc/navlateral.php"; ?>       
+
+        <!-- Page content -->
+        <section class="full-width pageContent scroll" id="pageContent">
+
+            <?php require_once "./app/views/inc/navbar.php"; ?>
+
+
+        </section> <!-- Fin page content -->
+
+    </main> <!-- Fin main container -->
+
+    <?php require_once "./app/views/inc/script.php"; ?>
+</body>
+</html>
