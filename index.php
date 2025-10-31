80e278d9478632aec933a3c89584deea04f57825
Carlos Alfaro <carlosalfaro.info@gmail.com>
2025-10-31 11:11:13 -0600

    Video #9

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
     require_once "./config/app.php";
+
+    require_once "./app/views/inc/session_start.php";
+
+    if(isset($_GET['views'])){
+        $url=explode("/",$_GET['views']);
+    }else{
+        $url=["login"];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="es">
