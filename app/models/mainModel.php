87032bf6284b3c459b0f1177c4da953c415ec91f
Carlos Alfaro <carlosalfaro.info@gmail.com>
2025-11-18 08:46:50 -0600

    Video #16

---
+++ app/models/mainModel.php
@@ -0,0 +1,31 @@
+<?php
+
+    namespace app\models;
+    use \PDO;
+
+    if(file_exists(__DIR__."/../../config/server.php")){
+        require_once __DIR__."/../../config/server.php";
+    }
+
+    class mainModel{
+
+        private $server=DB_SERVER;
+        private $db=DB_NAME;
+        private $user=DB_USER;
+        private $pass=DB_PASS;
+
+        /*----------  Funcion conectar a BD  ----------*/
+        protected function conectar(){
+            $conexion = new PDO("mysql:host=".$this->$server.";dbname=".$this->$db, $this->$user, $this->$pass);
+            $conexion->exec("SET CHARACTER SET utf8");
+
+            return $conexion;
+        }
+
+        /*----------  Funcion ejecutar consultas  ----------*/
+        protected function ejecutarConsulta($consulta){
+            $sql=$this->conectar()->prepare($consulta);
+            $sql->execute();
+            return $sql;
+        }
+    }
