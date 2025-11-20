07fe4ab5def8f9c9145453d587e0a3b4aff560c0
Carlos Alfaro <carlosalfaro.info@gmail.com>
2025-11-20 10:01:41 -0600

    Video #18

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# POS
+# SISTEMA DE VENTAS EN PHP y MYSQL (POS - FACTURACION)
 Codigo fuente del curso sistema de ventas en PHP y MySQL, MVC, POO, PDO
+
+# ▶️ CURSO COMPLETO EN YOUTUBE ▶️
+<a href="https://www.youtube.com/playlist?list=PLH_tVOsiVGzmLcoS0q9op-lqRjrKrQiPn" target="_blank">Ver curso en YouTube</a>
--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -28,4 +28,32 @@
             $sql->execute();
             return $sql;
         }
+
+        /*----------  Funcion limpiar cadenas  ----------*/
+        public function limpiarCadena($cadena){
+
+            $palabras=["<script>","</script>","<script src","<script type=","SELECT * FROM","SELECT "," SELECT ","DELETE FROM","INSERT INTO","DROP TABLE","DROP DATABASE","TRUNCATE TABLE","SHOW TABLES","SHOW DATABASES","<?php","?>","--","^","==",";","::","'or","1=1","'or 1=1"];
+
+            $cadena=trim($cadena);
+            $cadena=stripslashes($cadena);
+
+            foreach($palabras as $palabra){
+                $cadena=str_ireplace($palabra, "", $cadena);
+            }
+
+            $cadena=trim($cadena);
+            $cadena=stripslashes($cadena);
+
+            return $cadena;
+        }
+
+
+        /*---------- Funcion verificar datos (expresion regular) ----------*/
+        protected function verificarDatos($filtro,$cadena){
+            if(preg_match("/^".$filtro."$/",$cadena)){
+                return false;
+            }else{
+                return true;
+            }
+        }
     }
